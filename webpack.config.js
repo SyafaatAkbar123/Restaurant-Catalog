2165d92ec8d5a1a183b4ecca131e71c6262335b2
SyafaatAkbar123 <syafaatakbar123@gmail.com>
2024-05-31 01:02:51 +0700

    add project folders

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    open: true,
+    port: 9900,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public'),
+          to: path.resolve(__dirname, 'dist/public'),
+        },
+      ],
+    }),
+    new FaviconsWebpackPlugin({
+      /**
+       * Full documentations:
+       * https://www.npmjs.com/package/favicons-webpack-plugin#user-content-advanced-usage
+       */
+      logo: path.resolve(__dirname, 'src/public/kogu-logo.png'),
+
+      outputPath: 'public/favicons',
+
+      publicPath: 'public',
+      prefix: 'favicons/',
+    }),
+  ],
+};
